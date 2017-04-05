651a7bd6756cba4b9f58cff7ccb3ac6446e5a2c9
LilyCole <Lilia.Cole@gmail.com>
2017-04-04 21:17:39 -0700

    adds angular and shopstylecontroller

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -1,12 +1,24 @@
 console.log('Assets connected.');
 
-$.ajax({
-  url: 'https://api.viki.io/v4/search.json?c=b&per_page=5&with_people=true&with_paywall=1&app=100266a&t=1440586215',
-  success: function successCallback(response) {
-    var items = response;
-    console.log('data:',response);
-  },
-  error: function errorCallback(response) {
+// App
+angular
+  .module('shopStyleApp', [])
+  .controller('ShopStyleController', ShopStyleController);
+
+// Controller
+ShopStyleController.$inject = ['$http'];
+function ShopStyleController($http) { 
+  var vm = this;
+  vm.data;
+  console.log('hi');
+  $http({
+    method: 'GET',
+    url: 'https://api.viki.io/v4/search.json?c=b&per_page=5&with_people=true&with_paywall=1&app=100266a&t=1440586215'
+  }).then(function successCallback(response) {
+    vm.data = response.data;
+    console.log('data:',vm.data);
+    $('#change-me').html('nope');
+  }, function errorCallback(response) {
     console.log('There was an error getting the data', response);
-  }
-});
+  });
+}
