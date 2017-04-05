00395c04938f92473711c7f418071b752e89348a
LilyCole <Lilia.Cole@gmail.com>
2017-04-04 22:33:42 -0700

    gets data from #query-input without angular

--- public/assets/scripts/app.js
+++ public/assets/scripts/app.js
@@ -1,24 +1,30 @@
 console.log('Assets connected.');
 
-// App
-angular
-  .module('shopStyleApp', [])
-  .controller('ShopStyleController', ShopStyleController);
+// Change event when typing in #query-input
+var onload = function() {
+  var e = document.getElementById('query-input');
+  e.oninput = search;
+  e.onpropertychange = e.oninput;
+};
 
-// Controller
-ShopStyleController.$inject = ['$http'];
-function ShopStyleController($http) { 
-  var vm = this;
-  vm.data;
-  console.log('hi');
-  $http({
-    method: 'GET',
-    url: 'https://api.viki.io/v4/search.json?c=b&per_page=5&with_people=true&with_paywall=1&app=100266a&t=1440586215'
-  }).then(function successCallback(response) {
-    vm.data = response.data;
-    console.log('data:',vm.data);
-    $('#change-me').html('nope');
-  }, function errorCallback(response) {
+// Search with query value
+var search = function() {
+  var query = $('#query-input')[0].value;
+  var queryString = '?c=' + query + '&per_page=5&with_people=true&with_paywall=1&app=100266a&t=1440586215';
+  console.log('queryString',queryString);
+  $.ajax({
+    url: 'https://api.viki.io/v4/search.json' + queryString,
+    success: function successCallback(response) {
+      var data = response;
+      displayData(data);
+      console.log(data);
+    },
+    error: function errorCallback(response) {
     console.log('There was an error getting the data', response);
+    }
   });
+}
+
+var displayData = function(data) {
+  
 }
